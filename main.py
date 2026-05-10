f13d73c0f22ffee1b898b16da4aaa65131cd0d33
Apeksha mahadik <apekshamahadik18@gmail.com>
2026-05-10 18:49:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sys
+import subprocess
+
 from PySide6.QtWidgets import QApplication
 from PySide6.QtQml import QQmlApplicationEngine
 from PySide6.QtCore import QObject, Slot
-import subprocess
 
 
 class Backend(QObject):
@@ -24,13 +25,67 @@ class Backend(QObject):
             return e.output
 
 
+    @Slot(result=str)
+    def startContainer(self):
+
+        try:
+            output = subprocess.check_output(
+                "docker start nginx-container",
+                shell=True,
+                stderr=subprocess.STDOUT,
+                text=True
+            )
+
+            return output
+
+        except subprocess.CalledProcessError as e:
+            return e.output
+
+
+    @Slot(result=str)
+    def stopContainer(self):
+
+        try:
+            output = subprocess.check_output(
+                "docker stop nginx-container",
+                shell=True,
+                stderr=subprocess.STDOUT,
+                text=True
+            )
+
+            return output
+
+        except subprocess.CalledProcessError as e:
+            return e.output
+
+
+    @Slot(result=str)
+    def getLogs(self):
+
+        try:
+            output = subprocess.check_output(
+                "docker logs nginx-container",
+                shell=True,
+                stderr=subprocess.STDOUT,
+                text=True
+            )
+
+            return output
+
+        except subprocess.CalledProcessError as e:
+            return e.output
+
+
 app = QApplication(sys.argv)
 
 engine = QQmlApplicationEngine()
 
 backend = Backend()
 
-engine.rootContext().setContextProperty("backend", backend)
+engine.rootContext().setContextProperty(
+    "backend",
+    backend
+)
 
 engine.load("qml/Main.qml")
 
