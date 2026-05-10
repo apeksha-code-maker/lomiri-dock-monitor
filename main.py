9f137184ab3e96348fe2d0d19d11e945e893e7c0
Apeksha mahadik <apekshamahadik18@gmail.com>
2026-05-10 11:49:12 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import sys
+from PySide6.QtWidgets import QApplication
+from PySide6.QtQml import QQmlApplicationEngine
+from PySide6.QtCore import QObject, Slot
+import subprocess
+
+
+class Backend(QObject):
+
+    @Slot(str, result=str)
+    def runCommand(self, command):
+
+        try:
+            output = subprocess.check_output(
+                command,
+                shell=True,
+                stderr=subprocess.STDOUT,
+                text=True
+            )
+
+            return output
+
+        except subprocess.CalledProcessError as e:
+            return e.output
+
+
+app = QApplication(sys.argv)
+
+engine = QQmlApplicationEngine()
+
+backend = Backend()
+
+engine.rootContext().setContextProperty("backend", backend)
+
+engine.load("qml/Main.qml")
+
+if not engine.rootObjects():
+    sys.exit(-1)
+
+sys.exit(app.exec())
