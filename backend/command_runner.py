5e75e92c4b58a21a2c29fbb8ac48041122d41e00
Apeksha mahadik <apekshamahadik18@gmail.com>
2026-05-10 18:21:48 +0530

    Update command_runner.py

--- backend/command_runner.py
+++ backend/command_runner.py
@@ -1,6 +1,9 @@
 import subprocess
+from PySide6.QtCore import QObject, Slot
 
-class CommandRunner:
+class CommandRunner(QObject):
+
+    @Slot(str, result=str)
     def runCommand(self, command):
         try:
             result = subprocess.check_output(
@@ -14,3 +17,11 @@ class CommandRunner:
 
         except subprocess.CalledProcessError as e:
             return e.output
+
+    @Slot(result=str)
+    def startContainer(self):
+        return self.runCommand("docker start nginx-container")
+
+    @Slot(result=str)
+    def stopContainer(self):
+        return self.runCommand("docker stop nginx-container")
